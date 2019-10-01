597a7b1f6531680ec6375806d06a15e575c32429
zephyrol <morgenthaler.sebastien@gmail.com>
2019-10-01 21:09:08 +0200

    Adds projects files

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: seb
+ *
+ * Created on 30 septembre 2019, 20:52
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+}
+
