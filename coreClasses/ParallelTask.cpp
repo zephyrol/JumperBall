aefe41f687b5246f775b3ab2fae1babefb3b0e6c
zephyrol <morgenthaler.sebastien@gmail.com>
2020-08-31 23:04:10 +0200

    Continues ParallelTasks

---
+++ coreClasses/ParallelTask.cpp
@@ -0,0 +1,76 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ParallelTask.cpp
+ * Author: Morgenthaler S 
+ * 
+ * Created on 31 aout 2020, 19:30
+ */
+
+#include "ParallelTask.h"
+
+ParallelTask::ParallelTask (const std::function<bool()>& taskFunction,
+                            size_t numberOfTasks) :
+    _endOfTasksIsRequested(false),
+    _numberOfThreads(getNumberOfThreads(numberOfTasks)),
+    _numberOfTasks(numberOfTasks),
+    _treatedTasks(0),
+    _mutexInProgress(),
+    _mutexDone(),
+    _taskFunction(taskFunction),
+    _asyncTasks()
+{
+    _mutexInProgress.lock();
+    for (size_t i = 0; i < _numberOfThreads; ++i) {
+        _asyncTasks.push_back( std::async(std::launch::async, [this](){
+            threadFunction(_taskFunction);}));
+    }
+}
+
+void ParallelTask::runTasks()
+{
+    _mutexDone.lock();
+    _treatedTasks = 0;
+    _mutexInProgress.unlock();
+}
+
+void ParallelTask::threadFunction(const std::function<bool()>& task) {
+
+    while(!_endOfTasksIsRequested) {
+        _mutexInProgress.lock();
+        ++_treatedTasks;
+        if (_treatedTasks < _numberOfTasks) {
+            _mutexInProgress.unlock();
+            task();
+        }
+    }
+}
+
+
+size_t ParallelTask::getNumberOfThreads(size_t numberOfTasks)
+{
+    size_t numberOfThreads;
+    const size_t numberOfThreadsMax = std::thread::hardware_concurrency();
+    if (numberOfThreadsMax == 0) {
+        numberOfThreads = 1;
+    } else if (numberOfTasks < numberOfThreadsMax) {
+        numberOfThreads = numberOfTasks;
+    } else {
+        numberOfThreads = numberOfThreadsMax;
+    }
+    return numberOfThreads;
+}
+
+ParallelTask::~ParallelTask() {
+    _endOfTasksIsRequested = true;
+    for ( size_t i = 0; i < _numberOfThreads; ++i) {
+        _mutexInProgress.unlock();
+    }
+    for ( size_t i = 0; i < _numberOfThreads; ++i) {
+        _asyncTasks.at(i).wait();
+    }
+}
--- coreClasses/ParallelTask.h
+++ coreClasses/ParallelTask.h
@@ -8,21 +8,34 @@
 #ifndef PARALLEL_TASK_H 
 #define PARALLEL_TASK_H  
 #include "Types.h"
+#include "ClassicalMechanics.h"
 #include <future>
 
 class ParallelTask {
-    ParallelTask (const std::function<void()> task);
+    ParallelTask (const std::function<bool()>& taskFunction,
+                  size_t numberOfTasks );
     ~ParallelTask();
+
+public:
+    void runTasks();
+
+private:
     //--------ATTRIBUTES-----------//
     bool                    _endOfTasksIsRequested;
-    size_t                  _treatedComponents;
-    std::mutex              _mutexComponentsInProgress;
-    std::mutex              _mutexComponentsDone;
-    std::vector<std::future<void> > _asyncTasksComponent;
+    size_t                  _numberOfThreads;
+    size_t                  _numberOfTasks;
+    size_t                  _treatedTasks;
+    std::mutex              _mutexInProgress;
+    std::mutex              _mutexDone;
+    std::function<bool()>   _taskFunction;
+    std::vector<std::future<void> > _asyncTasks;
 
     //----------METHODS-------------//
-    void                    treatComponent();
+    void                 threadFunction(const std::function<bool()>& task);
     std::vector<std::future<void> >genAsyncTasks();
+
+    //------ STATIC METHODS---------//
+    static size_t getNumberOfThreads(size_t numberOfTasks);
 };
 
 #endif /* PARALLEL_TASK_H */
