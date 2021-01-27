b426d0aca8f11532a569c608a7968f9aa7767861
zephyrol <morgenthaler.sebastien@gmail.com>
2021-01-27 20:39:50 +0100

    remove useless headers

--- coreClasses/gameMenu/Page.cpp
+++ coreClasses/gameMenu/Page.cpp
@@ -2,11 +2,6 @@
 #include <math.h>
 #include <algorithm>
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /*
  * File:   Map.h
--- coreClasses/player/Player.cpp
+++ coreClasses/player/Player.cpp
@@ -1,16 +1,10 @@
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
+﻿
+/*
  * File:   Player.cpp
  * Author: Morgenthaler S 
  * 
  * Created on 10 mai 2020, 11:40
  */
-
 #include "Player.h"
 
 Player::Player() :
