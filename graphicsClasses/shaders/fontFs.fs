f6bdd65f26a80fab0643bf47c77cd98c33af221d
zephyrol <morgenthaler.sebastien@gmail.com>
2022-10-02 22:35:06 +0200

    fix: Fix font shader

--- graphicsClasses/shaders/fontFs.fs
+++ graphicsClasses/shaders/fontFs.fs
@@ -1,14 +1,12 @@
 uniform sampler2D characterTexture;
 
-#ifdef(SCROLLABLE)
-    uniform float levelProgression;
-#endif
-
 in vec3 fs_vertexColor;
 in vec2 fs_vertexUVs;
 in vec2 fs_labelType;
 
 out vec4 pixelColor;
+
+uniform float levelProgression;
 
 void main() {
 
@@ -17,13 +15,9 @@ void main() {
         : texture(characterTexture, vec2(fs_vertexUVs.x, fs_vertexUVs.y)).x;
 
     vec3 color;
-    #ifdef(SCROLLABLE)
-        color = fs_labelType.x == 1.0 && fs_labelType.y > 0.0 && fs_labelType.y > levelProgression
+    color = fs_labelType.x == 1.0 && fs_labelType.y > 0.0 && fs_labelType.y > levelProgression
             ? fs_vertexColor * 0.2
             : fs_vertexColor;
-    #else
-        color = fs_vertexColor;
-    #endif
 
     pixelColor = vec4(color, alpha);
 }
--- graphicsClasses/shaders/fontVs.vs
+++ graphicsClasses/shaders/fontVs.vs
@@ -1,7 +1,3 @@
-#ifdef(SCROLLABLE)
-    uniform float positionY;
-#endif
-
 layout(location = 0) in vec3 vs_vertexPosition;
 layout(location = 1) in vec3 vs_vertexColor;
 layout(location = 3) in vec2 vs_vertexUVs;
@@ -11,15 +7,16 @@ out vec2 fs_vertexUVs;
 out vec3 fs_vertexColor;
 out vec2 fs_labelType;
 
+uniform float positionY;
+
+
 void main() {
     fs_vertexUVs = vs_vertexUVs;
     fs_vertexColor = vs_vertexColor;
     fs_labelType = vs_labelType;
 
     vec2 positionXY = vs_vertexPosition.xy;
-    #ifdef(SCROLLABLE)
-        positionXY += vec2(0.0, positionY);
-    #endif
+    positionXY += vec2(0.0, positionY);
 
     gl_Position = vec4(positionXY, 0.0, 1.0);
 }
