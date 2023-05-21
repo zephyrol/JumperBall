4bee32cd892ee375d7c634a1d833ccbc6abdcf5c
Sebastien Morgenthaler <morgenthaler.sebastien@gmail.com>
2023-05-21 12:42:46 +0200

    feat: Change skin colors

--- graphicsClasses/shaders/labelFs.fs
+++ graphicsClasses/shaders/labelFs.fs
@@ -74,25 +74,66 @@ vec3 getBallColor() {
     float x = (fs_vertexUVs.x - 0.5) * 2.0;
     float y = (fs_vertexUVs.y - 0.5) * 2.0;
     float pixelDistance = sqrt(x * x + y * y);
+    // Check https://www.canva.com/colors/color-wheel/ to choose colors
     if(pixelDistance > 1.0) {
         discard;
     }
     if (fs_ball > 5.5) {
-
+        const float tetradricFirstThreshold = -0.5;
+        const float tetradricSecondThreshold = 0.0;
+        const float tetradricThirdThreshold = 0.5;
+        const vec3 tetradricFirstColor = vec3(34.0 / 255.0, 221.0 / 255.0, 213.0 / 255.0);
+        const vec3 tetradricSecondColor = vec3(119.0 / 255.0, 34.0 / 255.0, 221.0 / 255.0);
+        if(y > tetradricThirdThreshold) {
+            return tetradricFirstColor;
+        }
+        if(y > tetradricSecondThreshold) {
+            return tetradricSecondColor;
+        }
+        if(y > tetradricFirstThreshold) {
+            return vec3(1.0) - tetradricFirstColor;
+        }
+        return vec3(1.0) - tetradricSecondColor;
     }
+    const float triadicFirstThreshold = -1.0 / 3.0;
+    const float triadicSecondThreshold = -triadicFirstThreshold;
     if (fs_ball > 4.5) {
-
+        if(y > triadicSecondThreshold) {
+            const vec3 secondTriadicFirstColor = vec3(223.0 / 255.0, 142.0 / 255.0, 32.0 / 255.0);
+            return secondTriadicFirstColor;
+        }
+        if(y > triadicFirstThreshold) {
+            const vec3 secondTriadicSecondColor = vec3(32.0 / 255.0, 223.0 / 255.0, 142.0 / 255.0);
+            return secondTriadicSecondColor;
+        }
+        const vec3 secondTriadicThirdColor = vec3(142.0 / 255.0, 32.0 / 255.0, 223.0 / 255.0);
+        return secondTriadicThirdColor;
     }
     if (fs_ball > 3.5) {
-
+        if(y > triadicSecondThreshold) {
+            const vec3 firstTriadicFirstColor = vec3(198.0 / 255.0, 218.0 / 255.0, 37.0 / 255.0);
+            return firstTriadicFirstColor;
+        }
+        if(y > triadicFirstThreshold) {
+            const vec3 firstTriadicSecondColor = vec3(37.0 / 255.0, 198.0 / 255.0, 218.0 / 255.0);
+            return firstTriadicSecondColor;
+        }
+        const vec3 firstTriadicThirdColor = vec3(218.0 / 255.0, 37.0 / 255.0, 198.0 / 255.0);
+        return firstTriadicThirdColor;
     }
     if (fs_ball > 2.5) {
-
+        const vec3 thirdLightColor = vec3(64.0 / 255.0, 42.0 / 255.0, 213.0 / 255.0);
+        const vec3 thirdDarkColor = vec3(1.0) - thirdLightColor;
+        return y > 0.0 ? thirdLightColor : thirdDarkColor;
     }
     if (fs_ball > 1.5) {
-
+        const vec3 secondLightColor = vec3(44.0 / 255.0, 211.0 / 255.0, 208.0 / 255.0);
+        const vec3 secondDarkColor = vec3(1.0) - secondLightColor;
+        return y > 0.0 ? secondLightColor : secondDarkColor;
     }
-    return vec3(1.0);
+    const vec3 firstLightColor = vec3(32.0 / 255.0, 223.0 / 255.0, 123.0 / 255.0);
+    const vec3 firstDarkColor = vec3(1.0) - firstLightColor;
+    return  y > 0.0 ? firstLightColor : firstDarkColor;
 }
 
 #endif
