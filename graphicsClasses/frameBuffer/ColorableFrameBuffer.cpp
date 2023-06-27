14df0c569e38e10bf601b357ccf1ff3cade7d66f
Sebastien Morgenthaler <morgenthaler.sebastien@gmail.com>
2023-06-27 21:42:53 +0200

    perf: WIP use GL_nearest on hdr

--- graphicsClasses/frameBuffer/ColorableFrameBuffer.cpp
+++ graphicsClasses/frameBuffer/ColorableFrameBuffer.cpp
@@ -26,8 +26,8 @@ ColorableFrameBuffer_uptr ColorableFrameBuffer::createInstance(
         resolutionY
     );
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, isHDR ? GL_NEAREST : GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, isHDR ? GL_NEAREST : GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
