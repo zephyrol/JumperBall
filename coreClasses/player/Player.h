5c391ecb16016e93ebaaecff06808fea55e2a409
Sebastien Morgenthaler <morgenthaler.sebastien@gmail.com>
2023-09-02 20:29:42 +0200

    feat: Add new levels

--- coreClasses/player/Player.h
+++ coreClasses/player/Player.h
@@ -123,7 +123,7 @@ public:
      */
     bool hasBoughtSkin(size_t skinNumber) const;
 
-    static constexpr unsigned int maxLevel = 46;
+    static constexpr unsigned int maxLevel = 49;
 
     template<typename T>
     static T readValue(std::istringstream& stream);
