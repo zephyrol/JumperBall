89886eafad9ece8cec2937f1ef6244b02b095b17
Sebastien Morgenthaler <morgenthaler.sebastien@gmail.com>
2023-11-08 18:37:46 +0100

    Add new maps

--- coreClasses/player/Player.h
+++ coreClasses/player/Player.h
@@ -122,8 +122,7 @@ public:
      * @param skinNumber between [0-4]
      */
     bool hasBoughtSkin(size_t skinNumber) const;
-
-    static constexpr unsigned int maxLevel = 74;
+    static constexpr unsigned int maxLevel = 76;
 
     template<typename T>
     static T readValue(std::istringstream& stream);
