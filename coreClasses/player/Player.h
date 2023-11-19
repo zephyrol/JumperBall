418f7f25ac130650fd80d0806cf512723f68fe38
zephyrol <morgenthaler.sebastien@gmail.com>
2023-11-19 19:30:48 +0100

    feat: Add map

--- coreClasses/player/Player.h
+++ coreClasses/player/Player.h
@@ -122,7 +122,7 @@ public:
      * @param skinNumber between [0-4]
      */
     bool hasBoughtSkin(size_t skinNumber) const;
-    static constexpr unsigned int maxLevel = 79;
+    static constexpr unsigned int maxLevel = 80;
 
     template<typename T>
     static T readValue(std::istringstream& stream);
