b365420e387e6447e153ca1c8527d94efbd329c4
zephyrol <morgenthaler.sebastien@gmail.com>
2024-01-27 17:14:10 +0100

    update arrow orientations

--- coreClasses/gameMenu/labels/ArrowLabel.cpp
+++ coreClasses/gameMenu/labels/ArrowLabel.cpp
@@ -17,8 +17,32 @@ ArrowLabel::ArrowLabel(
 
 vecLabelGeometry ArrowLabel::genGeometries() const {
 
+    constexpr auto quadOffset = 0.25f;
+    constexpr auto quadWidth = 0.5f;
+    constexpr auto quadHeight = 0.3f;
+
+    constexpr auto triangleWidth = 0.5f;
+    constexpr auto triangleHeight = 0.6f;
+    if (_direction == JBTypes::Direction::West) {
+        return {
+            createDisplayableQuad(quadOffset, 0.f, quadWidth, quadHeight),
+            createDisplayableTriangle(0.f, 0.f, triangleWidth, triangleHeight, _direction),
+        };
+    }
+    if (_direction == JBTypes::Direction::East) {
+        return {
+            createDisplayableQuad(-quadOffset, 0.f, quadWidth, quadHeight),
+            createDisplayableTriangle(0.f, 0.f, triangleWidth, triangleHeight, _direction),
+        };
+    }
+    if (_direction == JBTypes::Direction::North) {
+        return {
+            createDisplayableQuad(0.f, quadOffset, quadHeight, quadWidth),
+            createDisplayableTriangle(0.f, 0.f, triangleHeight, triangleWidth, _direction),
+        };
+    }
     return {
-        createDisplayableQuad(0.25f, 0.f, 0.5f, 0.3f),
-        createDisplayableTriangle(0.f, 0.f, 0.5f, 0.6f, _direction),
+        createDisplayableQuad(0.f, -quadOffset, quadHeight, quadWidth),
+        createDisplayableTriangle(0.f, 0.f, triangleHeight, triangleWidth, _direction),
     };
 }
