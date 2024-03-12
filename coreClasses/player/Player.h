bd0375ee84a9b4d4123812784975612c51e85d9d
zephyrol <morgenthaler.sebastien@gmail.com>
2024-03-12 22:51:53 +0100

    feat: Add level

--- coreClasses/player/Player.h
+++ coreClasses/player/Player.h
@@ -122,7 +122,7 @@ public:
      * @param skinNumber between [0-4]
      */
     bool hasBoughtSkin(size_t skinNumber) const;
-    static constexpr unsigned int maxLevel = 115;
+    static constexpr unsigned int maxLevel = 116;
 
     template<typename T>
     static T readValue(std::istringstream& stream);
