42485c9442af8c7212a6379c76dd5f2b4b654ef1
zephyrol <morgenthaler.sebastien@gmail.com>
2024-06-04 20:55:08 +0200

    feat: Add sound when switch button is pressed

--- coreClasses/scene/blocks/special/SwitchButton.cpp
+++ coreClasses/scene/blocks/special/SwitchButton.cpp
@@ -6,31 +6,28 @@
  */
 #include "SwitchButton.h"
 
-SwitchButton::SwitchButton(
-    const JBTypes::Color &color,
-    const JBTypes::Dir &dir,
-    const JBTypes::vec3ui &position,
-    const Ball_sptr &ball,
-    bool isActivated
-) : InteractiveSpecial(color, position, dir, ball, isActivated) {
-}
+#include "system/SoundOutput.h"
+
+SwitchButton::SwitchButton(const JBTypes::Color& color,
+                           const JBTypes::Dir& dir,
+                           const JBTypes::vec3ui& position,
+                           const Ball_sptr& ball,
+                           bool isActivated)
+    : InteractiveSpecial(color, position, dir, ball, isActivated) {}
 
 void SwitchButton::applySpecialEffect() {
-    const auto &ball = _ball.lock();
-    for (const auto &block: *ball->getBlocksWithInteraction()) {
-        for (const auto &special: block->getSpecials()) {
-            if (
-                special->getColor() == getColor() ||
-                special->getColor() == JBTypesMethods::colorToShiny(getColor())
-                ) {
+    const auto& ball = _ball.lock();
+    _ball.lock()->addUpdateOutput(std::make_shared<SoundOutput>("switch"));
+    for (const auto& block : *ball->getBlocksWithInteraction()) {
+        for (const auto& special : block->getSpecials()) {
+            if (special->getColor() == getColor() ||
+                special->getColor() == JBTypesMethods::colorToShiny(getColor())) {
                 special->switchOnOff();
             }
         }
-        for (const auto &enemy: block->getEnemies()) {
-            if (
-                enemy->getColor() == getColor() ||
-                enemy->getColor() == JBTypesMethods::colorToShiny(getColor())
-                ) {
+        for (const auto& enemy : block->getEnemies()) {
+            if (enemy->getColor() == getColor() ||
+                enemy->getColor() == JBTypesMethods::colorToShiny(getColor())) {
                 enemy->switchOnOff();
             }
         }
@@ -39,30 +36,19 @@ void SwitchButton::applySpecialEffect() {
 
 vecCstShape_sptr SwitchButton::getShapes() const {
     const auto switchButtonShape = std::make_shared<const Shape>(
-        Shape::Aspect::Cylinder,
-        getColor(),
+        Shape::Aspect::Cylinder, getColor(),
         std::initializer_list<Transformation>(
-            {
-                Transformation(Transformation::Type::Scale, {0.4f, 0.075f, 0.4f}),
-                Transformation(
-                    Transformation::Type::Rotation,
-                    JBTypesMethods::rotationVectorUpToDir(direction())
-                )
-            }
-        )
-    );
+            {Transformation(Transformation::Type::Scale, {0.4f, 0.075f, 0.4f}),
+             Transformation(Transformation::Type::Rotation,
+                            JBTypesMethods::rotationVectorUpToDir(direction()))}));
 
     return {switchButtonShape};
 }
 
 Displayable::DynamicValues<JBTypes::vec3f> SwitchButton::getDynamicVec3fValues() const {
-    return {
-        positionF(),
-        isActivated() ? JBTypes::vec3f{1.f, 0.1f, 1.f} : JBTypes::vec3f{1.f, 1.f, 1.f}
-    };
+    return {positionF(), isActivated() ? JBTypes::vec3f{1.f, 0.1f, 1.f} : JBTypes::vec3f{1.f, 1.f, 1.f}};
 }
 
 Displayable::DynamicValues<JBTypes::Quaternion> SwitchButton::getDynamicQuaternionValues() const {
     return {{{0.f, 0.f, 0.f}, 1.f}};
 }
-
