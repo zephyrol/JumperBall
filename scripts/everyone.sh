1202d88e36ec4b4f632c769a928a183a96bb09f2
zephyrol <morgenthaler.sebastien@gmail.com>
2024-06-05 22:54:18 +0200

    feat: Apply personal modifications on levels

--- scripts/everyone.sh
+++ scripts/everyone.sh
@@ -2,7 +2,7 @@
 for i in `seq 1 $1`
 do
 	echo "Map $i"
-	bin/JumperBallApplication ../maps/map"$i"Uncompress.txt -compress
-	mv outMapNew.txt ../maps/map"$i".txt
+	build/build/Debug/GravityGlobe maps/map"$i"Uncompress.txt -compress
+	mv outMapNew.txt maps/map"$i".txt
 done
 ninja install
